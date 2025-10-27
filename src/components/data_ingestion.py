c696b61c956333eba564b8e7379ead7c7132ec10
sandaka charan <charan.sandaka5@gmail.com>
2025-10-27 14:26:57 +0530

    Data Transformation Done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation,DataTransformationConfig
+
 @dataclass # If we define decorator we dont need to use init to define a variable in a class
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts','train.csv')
@@ -47,5 +49,10 @@ class DataIngestion:
 if __name__ == "__main__":
     
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
     
+    data_transformation = DataTransformation()
+    train_path = 'artifacts/train.csv'
+    test_path = 'artifacts/test.csv'
+
+    data_transformation.initiate_data_transformation(train_path, test_path)
